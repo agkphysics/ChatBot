44cc89d01fe92ae112ee86f15bcacceb12cf9e47
Aaron Keesing <agkphysics@gmail.com>
2016-12-10 23:05:37 +1300

    Made XML processing concurrent and added description.

--- processXML.py
+++ processXML.py
@@ -1,10 +1,21 @@
+##
+## processXML.py - Script for processing the XMLs from the talkbank corpus
+##
+## You'll need to run the script with the -t option first to train the tagger
+## and chunker, and store them serialised.
+## 
+## Version: 1.5
+## Author: Aaron Keesing
+##
+
+
+
 #pylint: skip-file
 
 import xmltodict
 import nltk, nltk.corpus
 from nltk.parse.stanford import StanfordParser
 from nltk.tag.stanford import StanfordPOSTagger, StanfordNERTagger
-from sys import argv
 import os.path
 import os
 from itertools import groupby, zip_longest, cycle, islice
@@ -33,106 +44,15 @@ class TagChunker(nltk.ChunkParserI):
         return nltk.conllstr2tree('\n'.join(lines))
 
 
-parser = argparse.ArgumentParser(description='')
-parser.add_argument('infile', help="An input file or directory. If it is a file then the file is processed. " +
-                                    "If it is a directory then all .xml files will be processed recursively in " +
-                                    "the directory and all subdirectories.", type=str)
-parser.add_argument('-p', '--print', help="Print verbose info", action='store_true')
-parser.add_argument('-o', '--outputdir', help='The directory to place the processed file(s), default "xmlout/"', nargs='?', type=str, const='xmlout', default='')
-parser.add_argument('-t', '--train', help="Train the tagger and chunker and serialize them as files.", action='store_true')
-args = parser.parse_args()
-
-DEBUG = bool(args.print)
-if args.outputdir:
-    outputDir = args.outputdir
-    WRITEOUT = True
-else:
-    WRITEOUT = False
-infile = args.infile
-
-if WRITEOUT and not os.path.exists(outputDir):
-    os.makedirs(name=outputDir)
-
-files = [infile]
-if os.path.isdir(infile):
-    files = [str(p) for p in Path(argv[1]).glob('**/*.xml')]
-
-
-if args.train:
-    # Train POS tagger
-    train_sents = nltk.corpus.conll2000.tagged_sents()
-    patterns = [
-        (r'^[Yy]eah$', 'UH'),
-        (r'^[Oo][Hh]$', 'UH'),
-        (r'^[UuHh]+$', 'UH'),
-        (r'^[MmHh]+$', 'UH'),
-        (r'^[HhAa]+$', 'UH'),
-        (r'^okay$', 'JJ'),
-        (r'^[Oo][Kk]$', 'JJ'),
-
-        # The follow were copied from http://streamhacker.com/2008/11/10/part-of-speech-tagging-with-nltk-part-2/
-        (r'^-?[0-9]+(.[0-9]+)?$', 'CD'),
-        (r'.*ould$', 'MD'),
-        (r'.*ing$', 'VBG'),
-        (r'.*ed$', 'VBD'),
-        (r'.*ness$', 'NN'),
-        (r'.*ment$', 'NN'),
-        (r'.*ful$', 'JJ'),
-        (r'.*ious$', 'JJ'),
-        (r'.*ble$', 'JJ'),
-        (r'.*ic$', 'JJ'),
-        (r'.*ive$', 'JJ'),
-        (r'.*ic$', 'JJ'),
-        (r'.*est$', 'JJ'),
-        (r'^a$', 'IN'),
-    ]
-    from nltk.tag import brill, brill_trainer
-    templates = brill.brill24()
-    from time import time
-    startTime = time()
-    tagger = nltk.TrigramTagger(train_sents,
-                    backoff=nltk.BigramTagger(train_sents,
-                    backoff=nltk.UnigramTagger(train_sents,
-                    backoff=nltk.AffixTagger(train_sents,
-                    backoff=nltk.RegexpTagger(patterns)))))
-    trainer = brill_trainer.BrillTaggerTrainer(tagger, templates)
-    tagger = trainer.train(train_sents, max_rules=100, min_score=3)
-    endTime = time()
-    print('Trained tagger in {:.2f} secs'.format(endTime - startTime))
-    with open('tagger.pickle', 'wb') as taggerSerial:
-        pickle.dump(tagger, taggerSerial)
-    
-    # Train sentence chunker
-    train_chunks = nltk.corpus.conll2000.chunked_sents()
-    tag_sents = [nltk.tree2conlltags(tree) for tree in train_chunks]
-    tag_chunks = [[(t, c) for (w, t, c) in chunk_tags] for chunk_tags in tag_sents]
-    startTime = time()
-    chunktagger = nltk.BigramTagger(tag_chunks,
-                    backoff=nltk.UnigramTagger(tag_chunks))
-    chunker = TagChunker(chunktagger)
-    endTime = time()
-    print('Trained chunker in {:.2f} secs'.format(endTime - startTime))
-    with open('chunker.pickle', 'wb') as chunkerSerial:
-        pickle.dump(chunker, chunkerSerial)
-
-    print()
-else:
-    with open('tagger.pickle', 'rb') as taggerSerial:
-        tagger = pickle.load(taggerSerial)
-    print("Loaded tagger")
-    with open('chunker.pickle', 'rb') as chunkerSerial:
-        chunker = pickle.load(chunkerSerial)
-    print("Loaded chunker")
-
-
-for inFilename in files:
+def processFile(file):
+    inFilename = file
     print("Processing file {}".format(inFilename))
     with open(inFilename, encoding='utf8') as f:
         xmlDoc = xmltodict.parse(f.read(), encoding='utf-8')
         if (len(xmlDoc['CHAT']['@Lang'].split()) > 1
             or not isinstance(xmlDoc['CHAT']['Participants']['participant'], list)):
             # Ignore files with multiple languages or only one speaker
-            continue
+            return
 
         utters = []
         for utter in xmlDoc['CHAT']['u']:
@@ -215,3 +135,98 @@ for inFilename in files:
             outfile = open(outFilename, mode='bw')
             xmltodict.unparse(tree, output=outfile, pretty=True)
             outfile.close()
+
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('infile', help="An input file or directory. If it is a file then the file is processed. " +
+                                    "If it is a directory then all .xml files will be processed recursively in " +
+                                    "the directory and all subdirectories.", type=str)
+parser.add_argument('-p', '--print', help="Print verbose info", action='store_true')
+parser.add_argument('-o', '--outputdir', help='The directory to place the processed file(s), default "xmlout/"', nargs='?', type=str, const='xmlout', default='')
+parser.add_argument('-t', '--train', help="Train the tagger and chunker and serialize them as files.", action='store_true')
+args = parser.parse_args()
+
+DEBUG = bool(args.print)
+if args.outputdir:
+    outputDir = args.outputdir
+    WRITEOUT = True
+else:
+    WRITEOUT = False
+
+if WRITEOUT and not os.path.exists(outputDir):
+    os.makedirs(name=outputDir)
+
+if args.train:
+    # Train POS tagger
+    train_sents = nltk.corpus.conll2000.tagged_sents()
+    patterns = [
+        (r'^[Yy]eah$', 'UH'),
+        (r'^[Oo][Hh]$', 'UH'),
+        (r'^[UuHh]+$', 'UH'),
+        (r'^[MmHh]+$', 'UH'),
+        (r'^[HhAa]+$', 'UH'),
+        (r'^okay$', 'JJ'),
+        (r'^[Oo][Kk]$', 'JJ'),
+
+        # The follow were copied from http://streamhacker.com/2008/11/10/part-of-speech-tagging-with-nltk-part-2/
+        (r'^-?[0-9]+(.[0-9]+)?$', 'CD'),
+        (r'.*ould$', 'MD'),
+        (r'.*ing$', 'VBG'),
+        (r'.*ed$', 'VBD'),
+        (r'.*ness$', 'NN'),
+        (r'.*ment$', 'NN'),
+        (r'.*ful$', 'JJ'),
+        (r'.*ious$', 'JJ'),
+        (r'.*ble$', 'JJ'),
+        (r'.*ic$', 'JJ'),
+        (r'.*ive$', 'JJ'),
+        (r'.*ic$', 'JJ'),
+        (r'.*est$', 'JJ'),
+        (r'^a$', 'IN'),
+    ]
+    from nltk.tag import brill, brill_trainer
+    templates = brill.brill24()
+    from time import time
+    startTime = time()
+    tagger = nltk.TrigramTagger(train_sents,
+                    backoff=nltk.BigramTagger(train_sents,
+                    backoff=nltk.UnigramTagger(train_sents,
+                    backoff=nltk.AffixTagger(train_sents,
+                    backoff=nltk.RegexpTagger(patterns)))))
+    trainer = brill_trainer.BrillTaggerTrainer(tagger, templates)
+    tagger = trainer.train(train_sents, max_rules=100, min_score=3)
+    endTime = time()
+    print('Trained tagger in {:.2f} secs'.format(endTime - startTime))
+    with open('tagger.pickle', 'wb') as taggerSerial:
+        pickle.dump(tagger, taggerSerial)
+    
+    # Train sentence chunker
+    train_chunks = nltk.corpus.conll2000.chunked_sents()
+    tag_sents = [nltk.tree2conlltags(tree) for tree in train_chunks]
+    tag_chunks = [[(t, c) for (w, t, c) in chunk_tags] for chunk_tags in tag_sents]
+    startTime = time()
+    chunktagger = nltk.BigramTagger(tag_chunks,
+                    backoff=nltk.UnigramTagger(tag_chunks))
+    chunker = TagChunker(chunktagger)
+    endTime = time()
+    print('Trained chunker in {:.2f} secs'.format(endTime - startTime))
+    with open('chunker.pickle', 'wb') as chunkerSerial:
+        pickle.dump(chunker, chunkerSerial)
+    print()
+else:
+    with open('tagger.pickle', 'rb') as taggerSerial:
+        tagger = pickle.load(taggerSerial)
+    print("Loaded tagger")
+    with open('chunker.pickle', 'rb') as chunkerSerial:
+        chunker = pickle.load(chunkerSerial)
+    print("Loaded chunker")
+
+infile = args.infile
+if os.path.isdir(infile):
+    files = [str(p) for p in Path(infile).glob('**/*.xml')]
+    # Do tree concurrently
+    from concurrent.futures import ProcessPoolExecutor
+    with ProcessPoolExecutor() as executor:
+        executor.map(processFile, files)
+else:
+    processFile(infile)
